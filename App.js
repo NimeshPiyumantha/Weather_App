c252334640c838f11fc571de6d0c570e07550f23
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-17 15:42:12 +0530

    Update Content

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
