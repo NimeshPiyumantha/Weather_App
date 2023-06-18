d08e8c920eb7f5606921f58d17965d373395fd05
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-18 11:52:58 +0530

    Update Content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,20 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, SafeAreaView,Image } from "react-native";
-import {theme} from '../theme'
+import { SafeAreaView } from "react-native-safe-area-context";
+import { View, Text, Image, TextInput } from "react-native";
+import { theme } from "../theme";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 relative">
-      <StatusBar style="light"/>
-      <Image blurRadius={70} source={require('../assets/images/bg.png')}
-      className="absolute h-full w-full"
+      <StatusBar style="light" />
+      <Image
+        blurRadius={70}
+        source={require("../assets/images/bg.png")}
+        className="absolute h-full w-full"
       />
       <SafeAreaView className="flex flex-1">
         {/* Search Section */}
-        <View style={{height:'7%'}} className="mx-4 relative z-50">
-          <View className="flex-row justify-end item-center rounded-full" style={{backgroundColor: theme.bgWhite(0.2)}}>
-
+        <View style={{ height: "7%" }} className="mx-4 relative z-50">
+          <View
+            className="flex-row justify-end item-center rounded-full"
+            style={{ backgroundColor: theme.bgWhite(0.2) }}
+          >
+            <TextInput
+              placeholder="Search City"
+              placeholderTextColor={"lightgray"}
+              className="pl-6 h-10 flex-1 text-base text-white"
+            />
           </View>
         </View>
       </SafeAreaView>
