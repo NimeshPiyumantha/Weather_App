ed9abb820d6b89bee01e85209f90a7fb2507e0cc
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-18 11:39:35 +0530

    Update Content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,6 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, Text, SafeAreaView,Image } from "react-native";
+import {theme} from '../theme'
 
 export default function HomeScreen() {
   return (
@@ -9,6 +10,14 @@ export default function HomeScreen() {
       <Image blurRadius={70} source={require('../assets/images/bg.png')}
       className="absolute h-full w-full"
       />
+      <SafeAreaView className="flex flex-1">
+        {/* Search Section */}
+        <View style={{height:'7%'}} className="mx-4 relative z-50">
+          <View className="flex-row justify-end item-center rounded-full" style={{backgroundColor: theme.bgWhite(0.2)}}>
+
+          </View>
+        </View>
+      </SafeAreaView>
     </View>
   );
 }
