2b11e8293acae294d5b560633c8e5ebc4155fc35
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-18 11:09:18 +0530

    Update Content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,12 @@
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { View, Text, SafeAreaView } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <SafeAreaView>
-      <Text>HomeScreen</Text>
-    </SafeAreaView>
+    <View className="flex-1 relative">
+      <StatusBar style="light"/>
+      <Image source={require('../assets/images/bg.png')}
+    </View>
   );
 }
