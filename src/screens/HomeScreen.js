a6e52e200c17143f075f815533d01b1b81161cbe
NimeshPiyumantha <nimeshpiyumantha12@gmail.com>
2023-06-18 11:17:15 +0530

    Update Content

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView,Image } from "react-native";
 
 export default function HomeScreen() {
   return (
     <View className="flex-1 relative">
       <StatusBar style="light"/>
-      <Image source={require('../assets/images/bg.png')}
+      <Image blurRadius={70} source={require('../assets/images/bg.png')}
+      className="absolute h-full w-full"
+      />
     </View>
   );
 }
